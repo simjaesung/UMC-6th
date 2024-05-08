5bbceb3f3c307cc009f3ee2ec9bda2dbf32639f1
심재성 <simsorry@jaeseong.local>
2024-05-08 15:24:28 +0900

    5주차 m2 - 영화 찾기 기능 구현

--- 3~5주차/src/component/Card.jsx
+++ 3~5주차/src/component/Card.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 let MovieContent = styled.div`
   display: flex;
   flex-direction: column;
@@ -18,13 +18,14 @@ let MovieInfor = styled.div`
   padding-top: 10px;
   display: flex;
   justify-content: space-around;
-  font-size: 80%;
+  font-size: ${(props) => props.size || "70%"};
 `;
 
 let MovieHiddenInfor = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
+  font-size: ${(props) => props.size || "70%"};
   padding: 10px;
   opacity: 0.5;
   color: white;
@@ -37,7 +38,7 @@ let MovieHiddenInfor = styled.div`
   }
 `;
 
-function Card({movie}) {
+function Card({ movie, size }) {
   console.log(movie);
   let navigate = useNavigate();
   let [hover, showHover] = useState(false);
@@ -51,21 +52,21 @@ function Card({movie}) {
           showHover(false);
         }}
         onClick={() => {
-          navigate("/movies/" + movie.original_title, {state: movie});
+          navigate("/movies/" + movie.original_title, { state: movie });
         }}>
         {hover ? (
-          <MovieHiddenInfor>
+          <MovieHiddenInfor size={size}>
             <div>{movie.original_title}</div>
-            <div style={{paddingTop: "10px"}}>{movie.overview}</div>
+            <div style={{ paddingTop: "10px" }}>{movie.overview}</div>
           </MovieHiddenInfor>
         ) : (
           <>
             <MoviePoster>
               <img src={"https://image.tmdb.org/t/p/w500" + movie.poster_path} />
             </MoviePoster>
-            <MovieInfor>
-              <div style={{width: "60%"}}>{movie.original_title}</div>
-              <div style={{textAlign: "center"}}>⭐️ {movie.vote_average}</div>
+            <MovieInfor size={size}>
+              <div style={{ width: "60%" }}>{movie.original_title}</div>
+              <div style={{ textAlign: "center" }}>⭐️ {movie.vote_average}</div>
             </MovieInfor>
           </>
         )}
--- 3~5주차/src/page/MainPage.jsx
+++ 3~5주차/src/page/MainPage.jsx
@@ -18,10 +18,10 @@ let HomeSearch = styled.div`
   display: flex;
   color: white;
   background: black;
-  height: 100vh;
+  height: 90vh;
   align-items: center;
   flex-direction: column;
-  padding-top: 10%;
+  padding-top: 20px;
   font-size: 30px;
 `;
 
@@ -39,27 +39,53 @@ let Search = styled.input`
   border-radius: 20px;
 `;
 
+let MoviesContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  height: 100%;
+  width: 70%;
+  gap: 5px;
+  padding: 20px;
+  background: #31363f;
+  margin-top: 10px;
+  border-radius: 20px;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 12px; /* 스크롤바의 너비 */
+  }
+  /* 스크롤바 트랙 (바탕) 스타일 */
+  &::-webkit-scrollbar-track {
+    background: #eeeeee; /* 트랙의 배경색 */
+  }
+
+  /* 스크롤바 핸들 (움직이는 부분) 스타일 */
+  &::-webkit-scrollbar-thumb {
+    background: #76abae; /* 핸들의 배경색 */
+    border-radius: 6px; /* 핸들의 모서리 둥글기 */
+  }
+`;
+
 function Home() {
   const [movies, setMovies] = useState([]);
-  let [keyWord, setKeyword] = useState("");
+  let [query, setQuery] = useState("");
 
   const searchMovies = async (e) => {
     const apiKey = "5009ee1ef99802b8b3a1b0ccd69e274c";
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${keyWord}`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
 
     try {
       const response = await axios.get(url);
       setMovies(response.data.results);
+      console.log(movies);
     } catch (error) {
       console.error("검색 중 오류 발생:", error);
     }
   };
 
   useEffect(() => {
-    if (keyWord.trim()) {
-      searchMovies();
-    }
-  }, [keyWord]);
+    searchMovies();
+  }, [query]);
+
   return (
     <>
       <HomeBanner>
@@ -70,12 +96,18 @@ function Home() {
         <SearchBox>
           <Search
             onChange={(e) => {
-              setKeyword(e.target.value);
+              setQuery(e.target.value);
             }}
           />
           <IoSearch style={{ fontSize: "30px" }} />
         </SearchBox>
-        <ul>{keyWord ? movies.map((movie) => <Card movie={movie} />) : null}</ul>
+        {query.trim() ? (
+          <MoviesContainer>
+            {movies.map((movie) => (
+              <Card movie={movie} size={"40%"} key={movie.id} />
+            ))}
+          </MoviesContainer>
+        ) : null}
       </HomeSearch>
     </>
   );
